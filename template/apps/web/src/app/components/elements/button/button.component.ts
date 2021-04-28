fdef6aa4aeae00e6e3835bfc426ec67ce7ab40c7
Magnus Wolf <mwolf2706@live.de>
2021-04-28 14:40:24 +0200

    fixed button emitter

--- template/apps/web/src/app/components/elements/button/button.component.ts
+++ template/apps/web/src/app/components/elements/button/button.component.ts
@@ -1,19 +1,17 @@
-import { Component, EventEmitter, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'template-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
   @Input() route: string;
   @Input() inverse: boolean;
   @Input() title: string;
-  @Input() btnClick: EventEmitter<void>;
+  @Output() btnClick = new EventEmitter<void>();
 
   clickBtn(): void {
-    if (this.btnClick) {
-      this.btnClick.emit(null);
-    }
+    this.btnClick.emit();
   }
 }
